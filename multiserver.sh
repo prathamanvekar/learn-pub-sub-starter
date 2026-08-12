a70488c5a2ceadbd5081f5cb7e48903ca03f204a
Prathamesh Anvekar <anvekarprathamesh13@gmail.com>
2026-08-12 18:44:23 +0530

    comments added a bit and dockerfile

--- multiserver.sh
+++ multiserver.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 
 # Check if the number of instances was provided
+# the if condition checks if the string was empty, -z is a unary operator that returns true if the string that follows it has len of zero
+# $1 specifies the 1st argument and the "" around the $1 catches edge cases like whitespaces or missing spaces
 if [ -z "$1" ]; then
   echo "Usage: $0 <number-of-instances>"
   exit 1
 fi
 
+# saves the number of instances the user inputs
 num_instances=$1
 
 # Array to store process IDs
+# the -a attribute flag creates a 0-indexed array, in contrast with -A that creates the associative array with key-value structure
 declare -a pids
 
 # Function to kill all processes when Ctrl+C is pressed
 cleanup() {
   echo "Terminating all instances of ./cmd/server..."
+  # "${pids[@]}" $ the expansion operator | @ opens the array into distinct words 
   for pid in "${pids[@]}"; do
+    # the kill sends a signal toe sigterm(signal 15) asking the process to gracefully shutdown
     kill -SIGTERM "$pid"
   done
   exit
 }
 
 # Setup trap for SIGINT
+# trap is a built in command that sys signals and redirects them to proper actions
+# SIGINT: Short for Signal Interrupt (POSIX Signal 2), the standard signal sent by the terminal when a user presses Ctrl+C.
 trap 'cleanup' SIGINT
 
 # Start the specified number of instances of the program in the background
 for (( i=0; i<num_instances; i++ )); do
+  # &: The background operator. Sends it the background without blocking  
   go run ./cmd/server &
+  # $!: A special Bash variable containing the Process ID (PID) of the most recent background job launched by this shell
+  # (...): Array initialization syntax used when adding elements to Bash arrays.
   pids+=($!)
 done
 
--- rabbit.sh
+++ rabbit.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+# the bash function for starting/running the docker container
 start_or_run () {
+    # inspect docker command that is piped into nothing to hide it at channel two which is the std error into channel one which is already pointing to a black hole
     docker inspect peril_rabbitmq > /dev/null 2>&1
 
+    # the if statement checks if the last command ran successfully, if yes, it just starts the already there container
     if [ $? -eq 0 ]; then
         echo "Starting Peril RabbitMQ container..."
         docker start peril_rabbitmq
+    # if not runs a fresh container with proper arguments
     else
         echo "Peril RabbitMQ container not found, creating a new one..."
         docker run -d --name peril_rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
     fi
 }
 
+# this acts like a switch statement, looks for the second option cmd after our executable, options like start, stop, logs, if none, tells how to use
 case "$1" in
     start)
         start_or_run
